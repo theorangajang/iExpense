c298801eef4b4864753f184a735410e1638d4c6d
Alex Jang <alexjangspammyspam@gmail.com>
2020-12-21 10:48:20 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alex Jang on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
